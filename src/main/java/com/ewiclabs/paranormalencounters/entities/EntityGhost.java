4a477b9c349fcf77dd94c5a42fb31f06815d0a5f
ewicisbwitish <122370240+ewicisbwitish@users.noreply.github.com>
2024-02-16 11:37:08 -0700

    Updated files for ghost

--- src/main/java/com/ewiclabs/paranormalencounters/entities/EntityGhost.java
+++ src/main/java/com/ewiclabs/paranormalencounters/entities/EntityGhost.java
@@ -16,6 +16,15 @@ public class EntityGhost extends EntityLivingBase {
     // Define the behavior of the ghost
     public void onUpdate() {
         super.onUpdate();
+        if (this.world.isRemote && this.rand.nextInt(200) == 0) {
+            // Get a random sound event from the assets/sounds folder
+            ResourceLocation soundLocation = new ResourceLocation("paranormalencounters", "sounds/ghost_sound");
+            SoundEvent soundEvent = SoundEvent.REGISTRY.getObject(soundLocation);
+            if (soundEvent != null) {
+                // Play the sound at the entity's position
+                this.world.playSound(this.posX, this.posY, this.posZ, soundEvent, SoundCategory.HOSTILE, 1.0F, 1.0F, true);
+            }
+        }
         // Add code here to define the behavior of the ghost
     }
 }
